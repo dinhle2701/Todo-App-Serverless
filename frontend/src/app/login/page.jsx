d1e49dcb7dc91df8faf649d1d93c109f15cfa756
dinh-letat <dinhprococoa@gmail.com>
2025-05-19 16:07:55 +0700

    feat: add frontend

---
+++ frontend/src/app/login/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div className='login'>
+            <h1 className="text-3xl font-bold underline">
+                Login
+            </h1>
+        </div>
+    )
+}
+
+export default page
